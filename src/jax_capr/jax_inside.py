b277fcc77527610b4c7eabdac4cf49b6c2f3142c
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2025-10-22 18:00:50 +0900

    jax_capr の下地

---
+++ src/jax_capr/jax_inside.py
@@ -0,0 +1,10 @@
+from jax_rnafold.d0.ss import get_ss_partition_function
+
+# energy parameter class とかも import しておく必要ある。
+# submodules/jax-rnafold/src/jax_rnafold/d0/ss.py を参考にすれば良い
+# 算出される E, M, P などを使いたい。
+# OMM についてももしかしたら outside algorithm で利用することになりそうなのでとっておく。
+
+
+
+    
